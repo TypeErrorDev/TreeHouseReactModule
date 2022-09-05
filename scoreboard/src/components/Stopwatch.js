41ae231296697553dd6e28f4d5fe676b90f46a0d
TypeErrorDev <matthew.pantel86@gmail.com>
2022-09-05 14:29:59 +0200

    .

--- scoreboard/src/components/Stopwatch.js
+++ scoreboard/src/components/Stopwatch.js
@@ -1,15 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Stopwatch = () => {
   const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  //
+  useEffect(() => {
+    console.log("running side effect");
+    let id;
+
+    // set the interval if the stopwatch is running
+    if (isRunning) {
+      id = setInterval(() => {
+        setElapsedTime((prevTime) => prevTime + 1);
+        console.log("tick");
+      }, 1000);
+    }
+
+    // cleanup function to clear the interval
+    return () => {
+      clearInterval(id);
+      console.log("clean up");
+    };
+  }, [isRunning]);
+
   return (
     <div className="stopwatch">
       <h2>Stopwatch</h2>
-      <span className="stopwatch-time">0</span>
+      <span className="stopwatch-time">{elapsedTime}</span>
       <button onClick={() => setIsRunning((prevValue) => !prevValue)}>
         {isRunning ? "Stop" : "Start"}
       </button>
-      <button>Reset</button>
+      <button onClick={() => setElapsedTime(0)}>Reset</button>
     </div>
   );
 };
